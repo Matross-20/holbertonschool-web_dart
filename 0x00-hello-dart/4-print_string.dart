299d1690eb23faf7409338cfc81af9d4eda5001d
Matross-20 <matrossmthawelanga@gmail.com>
2025-07-08 16:05:34 +0200

    Printing Strings

--- 0x00-hello-dart/4-print_string.dart
+++ 0x00-hello-dart/4-print_string.dart
@@ -1,5 +1,5 @@
 void main() {
   const str = 'Holberton School';
-  print(str + str + str); // prints three times
+  print(str*3); // prints three times
   print("Holberton\n"); // single trailing newline
 }
