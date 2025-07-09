9704fc0228e052f57949edc0dd06a5ad194c8c77
Matross-20 <matrossmthawelanga@gmail.com>
2025-07-09 16:33:34 +0200

    recursive function to compute the factorial of a positive integer

--- 0x01-dart-function/3-factors.dart
+++ 0x01-dart-function/3-factors.dart
@@ -1,9 +1,9 @@
 int fact(int f) {
   if (f == 1) {
-      return 1;
-    }
+    return 1;
+  }
   if (f <= 0) {
-      return 0;
-    }
-    return f * fact(f - 1);
+    return 0;
+  }
+  return f * fact(f - 1);
 }
--- 0x01-dart-function/3-main.dart
+++ 0x01-dart-function/3-main.dart
@@ -1,7 +1,7 @@
 import '3-factors.dart';
 
 void main() {
-    print(fact(0));
-    print(fact(1));
-    print(fact(5));
+  print(fact(0));
+  print(fact(1));
+  print(fact(5));
 }
