a123ef49f0d9fb88d05dde0a5677fc688506e91a
Matross-20 <matrossmthawelanga@gmail.com>
2025-07-16 16:34:12 +0200

    a function longestUniqueSubstring() that accepts one argument String and returns the longest non-repeating substring

--- 0x01-dart-function/8-longest-unique-substring.dart
+++ 0x01-dart-function/8-longest-unique-substring.dart
@@ -1,25 +1,30 @@
 String longestUniqueSubstring(String str) {
-    Map<String, int> CharsMap = Map<String, int>();
-    int cursor = 0;
-    int startIndex = 0;
-    int maxLength = 0;
+  Map<String, int> charsMap = {}; //  Define the character map
+  int start = 0;
+  int maxLength = 0;
+  int cursor = 0;
+  String longest = '';
 
-    for (int i = 0; i < str.length; i++) {
-        String currentChar = str[i];
-        bool isExistingCharInMap =
-            charsMap[currentChar] != null && charsMap[currentChar]! >= cursor;
+  for (int i = 0; i < str.length; i++) {
+    String currentChar = str[i];
 
-        if (isExistingCharInMap) {
-            cursor = charsMap[currentChar] + 1;
-        }
-
-        charsMap[currentChar] = i;
-        bool isLongestNewString = maxLength < i - cursor +1;
-
-        if (isLongerNewString) {
-            maxLength = i -cursor;
-            startIndex = cursor;
-        }
+    //  If char was seen and is within the current window
+    if (charsMap.containsKey(currentChar) && charsMap[currentChar]! >= cursor) {
+      cursor = charsMap[currentChar]! + 1;
     }
-    return str.substring(startIndex, startIndex + maxLength);
-} 
+
+    charsMap[currentChar] = i;
+
+    int windowLength = i - cursor + 1;
+    if (windowLength > maxLength) {
+      maxLength = windowLength;
+      longest = str.substring(cursor, i + 1);
+    }
+  }
+
+  return longest;
+}
+
+void main() {
+  print(longestUniqueSubstring("holbertonschool")); // Output: "tonsch"
+}
